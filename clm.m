1bb9a48fbf02ffc402aef705e68c45460f53bc7b
xSanYay <145270952+xSanYay@users.noreply.github.com>
2024-04-05 01:00:10 +0530

    Update clm.m

--- clm.m
+++ clm.m
@@ -1,15 +1,10 @@
-% EXPERIMENT 2 CENTRAL LIMIT THEOREM
-% Experiment 2 (CLT)
-% Parameters
 population_size = 10000;
 num_samples = 1000;
 sample_size = 30;
 
-% Defining the population array
 population = exprnd(2, 1, population_size);
 sample_means = zeros(1, num_samples);
 
-% For loop to iteratively compute the sample means and append to the array
 for i = 1 : num_samples
     for i = 1 : num_samples
         sample = randsample(population, sample_size, true);
@@ -17,13 +12,9 @@ for i = 1 : num_samples
     end
 end
 
-% Plotting the population
 figure(1);
 subplot(2, 1, 1);
 histogram(population, 'Normalization', 'pdf', 'EdgeColor', 'none');
-title("Histogram of the population");
-xlabel("value");
-ylabel("probability density");
 
 subplot(2, 1, 2);
 histogram(sample_means, 'Normalization', 'pdf', 'EdgeColor', 'none');
@@ -31,7 +22,6 @@ title("Sample means of the population");
 xlabel("sample");
 ylabel("Sample means");
 
-% Deriving the PDF of the sample means and plotting them
 mu_population = mean(population);
 sigma_population = std(population);
 expected_mean = mean(sample_means);
