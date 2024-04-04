b23d862145482fbd0e1d9f073d36f367f10f3370
xSanYay <145270952+xSanYay@users.noreply.github.com>
2024-04-05 01:26:42 +0530

    Update dm.m

--- dm.m
+++ dm.m
@@ -1,19 +1,15 @@
-% Delta Modulation
 
-% Signal parameters
 a = 2;
 t = 0:2*pi/50:2*pi;
 x = a * sin(t);
 
-% Plot original signal
 plot(t, x, 'r');
 hold on;
 
-% Delta modulation parameters
 delta = 1;
 xn = zeros(size(x));
 
-% Apply delta modulation
+
 for i = 1:length(x)
     if x(i) > xn(i)
         d(i) = 1;
@@ -24,7 +20,41 @@ for i = 1:length(x)
     end
 end
 
-% Plot delta-modulated signal
+for i = 1:Lxsig
+    if (xsig(i) > sum)
+        if (cnt1 < 2)
+            sum = sum + delta;
+        elseif (cnt1 == 2)
+            sum = sum + 2 * delta;
+        elseif (cnt1 == 3)
+            sum = sum + 4 * delta;
+        else
+            sum = sum + 8 * delta;
+        end
+        if (sum < xsig(i))
+            cnt1 = cnt1 + 1;
+        else
+            cnt1 = 0;
+        end
+    elseif (cnt2 < 2)
+        sum = sum - delta;
+    elseif (cnt2 == 2)
+        sum = sum - 2 * delta;
+    elseif (cnt2 == 3)
+        sum = sum - 4 * delta;
+    else
+        sum = sum - 8 * delta;
+    end
+    if (sum > xsig(i))
+        cnt2 = cnt2 + 1;
+    else
+        cnt2 = 0;
+    end
+    ADMout(i) = sum;
+end
+
+t_downsampled = 0:ts:5;
+
 stairs(t, xn(1:end-1)); % Adjusted to ensure correct length
 legend('Analog signal', 'DM with step size=0.2');
 title('Delta Modulation');
